d78718081ec2755c7a92b26ca7585033968e5523
Denys Kuksin <deniskuksin1@gmail.com>
2025-02-09 12:26:53 +0200

    Add animator, add player script, set up player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    NavMeshAgent myNavMeshAgent;
+    Animator animator;
+
+    void Start()
+    {
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        Debug.Log(myNavMeshAgent.hasPath);
+
+        if (myNavMeshAgent.hasPath == true)
+        {
+            //Відбуватись анімація ходьби
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetDestinationToMousePosition();
+        }
+    }
+
+    void SetDestinationToMousePosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            myNavMeshAgent.SetDestination(hit.point);
+        }
+    }
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    //Коли телепортувати? (дія) - Виконано
+    //Куди телепортувати? (властивість) - Виконано
+    //Кого телепортувати? - Виконано
+    //Як телепортувати? 
+
+    //public Тип_даних Назва;
+    public GameObject Teleport1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = Teleport1.transform.position;
+    }
+}
